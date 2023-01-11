276d5a4de2132f8467d656f33185012b2f5e7e2c
Dillon <dillon@Dillonui-MacBookPro.local>
2023-01-12 01:20:56 +0900

    db connect url

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,11 +16,11 @@ import { VisitModule } from './daks/visit/visit.module';
       ignoreEnvFile: process.env.NODE_ENV === 'production',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('development', 'production').required(),
-        DB_HOST: Joi.string().required(),
-        DB_PORT: Joi.string().required(),
-        DB_USERNAME: Joi.string().required(),
-        DB_PASSWORD: Joi.string().required(),
-        DB_NAME: Joi.string().required(),
+        DB_HOST: Joi.string(),
+        DB_PORT: Joi.string(),
+        DB_USERNAME: Joi.string(),
+        DB_PASSWORD: Joi.string(),
+        DB_NAME: Joi.string(),
       }),
     }),
     TypeOrmModule.forRoot({
